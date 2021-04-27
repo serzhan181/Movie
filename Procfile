e20008fbedc01bd43f6786238ca6bd5fa593b544
lektorcode <lektorcode@yandex.com>
2021-04-27 14:47:20 +0600

    sadf

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: cd build/ && node index.js
--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,8 @@
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const prodConfig = process.env.DATABASE_URL
 
+console.log(devConfig)
+
 module.exports = {
   type: 'postgres',
   url: process.env.NODE_ENV === 'production' ? prodConfig : devConfig,
@@ -11,7 +13,6 @@ module.exports = {
   entities: ['src/entity/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
-  seeds: ['src/seeds/**/*{.ts,.js}'],
   cli: {
     entitiesDir: 'src/entity',
     migrationsDir: 'src/migration',
