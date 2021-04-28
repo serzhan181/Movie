d2c5eb257fdc7d1e59a230b7fa7e95ba820d9b5e
lektorcode <lektorcode@yandex.com>
2021-04-28 12:54:08 +0600

    deploy server

--- build/index.js
+++ build/index.js
@@ -10,7 +10,6 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const dotenv_1 = __importDefault(require("dotenv"));
-const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 5000;
 const app = express_1.default();
@@ -18,17 +17,14 @@ app.use(express_1.default.json());
 app.use(cookie_parser_1.default());
 app.use(cors_1.default({
     credentials: true,
-    origin: '*',
+    origin: process.env.ORIGIN,
     optionsSuccessStatus: 200,
 }));
-app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'client/build')));
-app.use(express_1.default.static('public'));
 // ROUTES
 app.use('/auth', routes_1.authRouter);
 app.use('/user', routes_1.userRoute);
 app.use('/posts', routes_1.postsRoute);
 app.use('/misc', routes_1.miscRoute);
-app.get('*', (_, res) => res.sendFile(path_1.default.join(__dirname, '..', 'client/build/index.html')));
 // CONNECTION
 typeorm_1.createConnection()
     .then(() => {
