7c72771eefe2372566ec989dec615d088d7bc222
lektorcode <lektorcode@yandex.com>
2021-04-28 12:46:50 +0600

    config

---
+++ client/src/api/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  THEMOVIEDB_TOKEN:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODE5ZGQxNmFkMTI5OTE0Yjg5YzE0NzgxYjg3YzhlNSIsInN1YiI6IjYwMmY4NjhkMzJjYzJiMDAzZjg0YTQxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nQipuoOB_WLYVhqgT1zFsag02JqoYdVFExI1HlbwfAs',
+}
+
+export const imgtypes = {
+  xl: 'https://image.tmdb.org/t/p/original/',
+  md: 'https://image.tmdb.org/t/p/w300/',
+  sm: 'https://image.tmdb.org/t/p/w200/',
+}
